119af1ce9d963d0d8ae9d4465af604a6bd531cad
Emtiaz Hossain Sami <samiemtiaz38@gmail.com>
2026-06-04 12:10:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-const earthPic = document.getElementById('earth-pic');
-let currentAngle = 0;
+const earth = document.getElementById('rotating-earth');
+let angle = 0;
 
-// ঘোরার স্পিড (ধীরে বা জোরে ঘোরাতে চাইলে ০.১৫ পরিবর্তন করতে পারেন)
-const speed = 0.15; 
-
-function rotateEarth() {
-    currentAngle += speed;
-    
-    // শুধু নিচের ইমেজটিকে ঘোরানো হচ্ছে, ওপরের আলোর প্রলেপটি স্থির থাকবে
-    earthPic.style.transform = `rotate(${currentAngle}deg)`;
-    
-    // মাখনের মতো স্মুথ অ্যানিমেশনের জন্য ব্রাউজার ফ্রেম রিকোয়েস্ট
-    requestAnimationFrame(rotateEarth);
+function animate() {
+    angle += 0.2; // ঘূর্ণন গতি কমাতে চাইলে এই মান আরও কমান
+    earth.style.transform = `rotate(${angle}deg)`;
+    requestAnimationFrame(animate);
 }
 
-// অ্যানিমেশন চালু করা হলো
-rotateEarth();
+animate();
